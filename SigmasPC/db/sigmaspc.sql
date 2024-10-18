ab58a8781f6dcceb487648433e3147a61c90fb94
PrasstyoAdhi2 <prasstyoadhi00@gmail.com>
2024-10-18 10:56:33 +0800

    add

--- SigmasPC/db/sigmaspc.sql
+++ SigmasPC/db/sigmaspc.sql
@@ -5,8 +5,9 @@ CREATE TABLE `products` (
   `image` varchar(255) NOT NULL,
   `category` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 INSERT INTO products VALUES ('52','Intel CoreTM i9-14900K New Gaming Desktop Processor 24 (8 P-cores + 16 E-cores) with Integrated Graphics - Unlocked','547.97','Intel i9.jpg','CPU');
 INSERT INTO products VALUES ('53','MSI Gaming GeForce RTX 3060 12GB 15 Gbps GDRR6 192-Bit HDMI/DP PCIe 4 Torx Twin Fan Ampere OC Graphics Card','284.99','Geforce rtx.jpg','Video Card');
 INSERT INTO products VALUES ('54','AMD Ryzen 9 5900X 12-core, 24-Thread Unlocked Desktop Processor','569.99','Ryzen.jpg','CPU');
+INSERT INTO products VALUES ('55','vga','1999.00','RobloxScreenShot20240919_134922603.png','Video Card');
