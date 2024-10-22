a81266f95d11873d294fae253d55f41125c6b378
JeremyRevaldoLP <jeremyrevaldo0000@gmail.com>
2024-10-22 17:13:58 +0800

    Change the title of every page from "PC component store" to "Sigmas PC"

--- SigmasPC/add_product.php
+++ SigmasPC/add_product.php
@@ -153,7 +153,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Product | PC Component Store</title>
+    <title>Add Product(ADMIN) | Sigmas PC</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <script>
--- SigmasPC/search.php
+++ SigmasPC/search.php
@@ -81,7 +81,7 @@ if (!empty($budget) || !empty($search_query) || !empty($categories)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search | PC Component Store</title>
+    <title>Search | Sigmas PC</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/stylesearch.css">
 </head>
