55c6f709b9f3f61b293fa59cf1e0ecf725f29d53
JeremyRevaldoLP <jeremyrevaldo0000@gmail.com>
2024-10-28 03:21:33 +0800

    Changes: - search.php and checkout.php have its own css file now
    - Its now easier to customize the css for the web
    
    - Added a lot of components(products)

--- SigmasPC/checkout.php
+++ SigmasPC/checkout.php
@@ -60,7 +60,7 @@ if (!empty($selectedProducts)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout | Sigmas PC</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/stylesearch.css">
+    <link rel="stylesheet" href="css/stylesearchandcheckout.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
